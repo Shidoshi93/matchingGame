4a1496ed293746bce3f357274508d40b7a1fed66
daniel.sacramento <daniel.sacramento@meta.com.br>
2021-11-01 17:45:04 +0000

    :construction: base componente card-game
    cria base do componente card-game

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+const CardGame = (name) => {
+    return `
+        <div class="card-game">
+            <img src="images/alura-pixel.png" class="icon-alura" alt="logo do alura"/>
+        </div>
+    `
+}
+export default CardGame
