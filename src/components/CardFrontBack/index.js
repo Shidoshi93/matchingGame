66e70d06a5042d9ab6f4cb26891831d0e211425b
daniel.sacramento <daniel.sacramento@meta.com.br>
2021-11-02 20:56:37 +0000

    :sparkles: add comportamento de flip no card

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,8 +2,18 @@ import './style.css'
 import CardGame from "../CardGame"
 
 const CardFrontBack = () => {
+
+    window.cardFrontBack = {}
+
+    window.cardFrontBack.hendleClick = (event) => {
+        const $origin = event.target
+        const $cardFrontBack = $origin.closest('.card-front-back')
+
+        $cardFrontBack.classList.toggle('-active')
+    }
+
     return `
-        <div class="card-front-back">
+        <div class="card-front-back" onclick="cardFrontBack.hendleClick(event)">
             <div class="card -front">
                 ${CardGame("alura-pixel", "logo alura")}
             </div>
